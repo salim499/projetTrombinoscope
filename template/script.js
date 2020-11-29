7d0ef4619d432e5d59be50e5744ffc28d1269334
salim499 <55392107+salim499@users.noreply.github.com>
2020-11-29 22:07:13 +0100

    Update script.js

--- template/script.js
+++ template/script.js
@@ -5,6 +5,7 @@ fetch("../data/items.json")
    return response.json();
 })
 .then(data => console.log(data));
+/*
 function Filter(e){
     let dom=document.getElementById('dom')
     dom.innerHTML=""
@@ -71,3 +72,102 @@ function Filter(e){
     }   
 }
 Filter(null)
+*/
+
+
+let equipes=[]
+let joueurs=[]
+let stades=[]
+let arbitres=[]
+let competitions=[]
+let dataJsons=[]
+
+
+function remplirDom(data){
+  let dom=document.getElementsByClassName("space")[0]
+  dom.innerHTML=""
+  data.forEach(element => {
+    let dom=document.getElementsByClassName("space")[0]
+    dom.innerHTML=dom.innerHTML+
+    `
+    <div class="card" style="width: 400px;">
+        <img src=${element["foaf:img"][0]['@value']} class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${element["dcterms:title"][0]['@value']}</h5>
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">
+            <a href="#" class="card-link">
+            Type : ${element["@type"][1].split(":")[1]}
+          </a>
+            </li>
+          <li class="list-group-item">
+            <a href="#" class="card-link">
+            </a>
+            </li>
+        </ul>
+        <div class="card-body">
+          <li class="list-group-item">
+          <a href="#" class="card-link">..............</a>
+        </li>
+        </div>
+      </div>
+    `
+  })
+}
+
+//let linkApi = "http://localhost/omeka/api/items";
+
+async function recupererData(){
+
+//let data= await fetch(linkApi)
+
+//let dataJson=await data.json()
+
+dataJson=itemsJson
+
+remplirDom(dataJson)
+
+dataJson.forEach(element => {
+
+  if(element["@type"][1].split(":")[1]=="Athlete"){
+    joueurs.push(element)
+  }
+  else if(element["@type"][1].split(":")[1]=="SportsTeamSeason"){
+    equipes.push(element)
+  }
+  else if (element["@type"][1].split(":")[1]=="Referee"){
+    arbitres.push(element)
+  } else if (element["@type"][1].split(":")[1]=="FootballMatch"){
+    competitions.push(element)
+  } else if (element["@type"][1].split(":")[1]=="Stadium") {
+    stades.push(element)
+  }
+  dataJsons.push(element)
+  });
+}   
+//}
+recupererData()
+
+function Filter(e){
+
+  if(e=="equipes"){
+    console.log(equipes)
+    remplirDom(equipes)
+  } else if(e=="arbitres"){
+    console.log(arbitres)
+    remplirDom(arbitres)    
+  } else if(e=="stades"){
+    console.log(stades)
+    remplirDom(stades)
+  } else if(e=="joueurs"){
+    console.log(joueurs)
+    remplirDom(joueurs)
+  } else if(e=="competitions"){
+    console.log(competitions)
+    remplirDom(competitions)
+  } else{
+    console.log(e)
+    remplirDom(dataJsons)
+  }
+}
